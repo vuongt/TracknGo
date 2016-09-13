8f956d3a2b9937ab2ad96d1ced73004a375d4358
tuyet <thi.vuong@student.ecp.fr>
2016-09-13 17:52:51 +0200

    Simulation d'une page d'authentification et connection avec une base mariadb

--- server/server.js
+++ server/server.js
@@ -2,12 +2,38 @@ var http = require("http");
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var Client = require("mariasql");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var mariaClient = new Client({
+	host:'localhost',
+	user:'root',
+	password:'digitalLab',
+	db:'app'
+});
 
 app.use(bodyParser.json());
 app.get("/", function(req, res){
 	res.setHeader('Content-Type','text/html');
-	res.send("<p>Hello there</p>");
-	
+	res.send("<strong>Hello there</strong>");
+})
+.get("/auth",function(req,res){
+	res.setHeader('Content-Type','text/html');
+	res.render('authentification.ejs',{});
+})
+.post("/auth/new", urlencodedParser, function(req,res){
+	const user = req.body;
+	console.log(req.body);
+	var prep = mariaClient.prepare('INSERT INTO users (name,email) VALUES (:name,:email)');
+	if (user != {}){
+			mariaClient.query(prep({name:user.name, email:user.email}),function(err, rows) {
+  		if (err)
+    		throw err;
+  		console.dir(rows);
+		});
+	}
+	mariaClient.end();
+	res.send("Inscription finished !");
 });
 
 app.listen(8080,function(){
