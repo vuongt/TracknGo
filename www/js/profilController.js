01d1b2c2050d3e33fdaac9859bd16f3c2b01200b
Valentin <valentin.louvet@supelec.fr>
2016-09-14 15:09:52 +0200

    amélioration profil

--- www/js/profilController.js
+++ www/js/profilController.js
@@ -1,5 +1,7 @@
 angular.module('starter.controllers')
   .controller('ProfilCtrl', function($scope) {
     $scope.name = "Louise Legall";
+    $scope.auteurs = ["auteur 1", "auteur 2","auteur 3", "auteur 4"];
+    $scope.titres = ["titre 1", "titre 2","titre 3"];
 
   });
