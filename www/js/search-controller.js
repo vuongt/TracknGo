1ca81d4d93532c09043724db16b7b26db41749ed
Valentin <valentin.louvet@supelec.fr>
2016-09-16 11:01:59 +0200

    correction controllers

--- www/js/search-controller.js
+++ www/js/search-controller.js
@@ -1,5 +1,27 @@
 angular.module('starter.controllers')
-  .controller('ProfilCtrl', function($scope) {
+  .controller('SearchCtrl', function($scope) {
+    $scope.concerts = [
+      {
+        titre : "titre 1",
+        date : "date 1",
+        adresse: "adresse 1",
+        show: false
+      }
+      ,
+      {
+        titre : "titre 2",
+        date : "date 2",
+        adresse: "adresse 2",
+        show: false
+      },
+      {
+        titre : "titre 3",
+        date : "date 3",
+        adresse: "adresse 3",
+        show: false
+      }
+    ];
+    $scope.auteurs = ["auteur 1", "auteur 2", "auteur 3"];
 
 
 
