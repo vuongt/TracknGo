eac79a25689d6957359b76acc318b46a523c417a
eymardhoudeville <eymard.houdeville@student.ecp.fr>
2016-09-19 10:52:22 +0200

    début page recherche avec barre en haut

--- www/js/artist-controller.js
+++ www/js/artist-controller.js
@@ -1,11 +1,14 @@
-angular.module('starter.controllers').controller('ArtistCtrl', function($scope) {
+angular.module('starter.controllers').controller('ArtistCtrl', function($scope, $http) {
 
   $scope.name="The Dumplings";
   A=["titre1", "titre2"];
   B=["titre3", "titre4"];
   $scope.albums=[{"name":"album1", "titres": A}, {"name":"album2", "titres": B}];
 
+	$http.get('http://localhost:8080/artist?name=STROMAE').then(function(res){
+    console.log(res.data);
 
+			})
 
   /*
    * if given group is the selected group, deselect it
