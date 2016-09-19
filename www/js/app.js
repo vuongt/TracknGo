15be3b00f6a8b6c75dca9826e487eb8f1e53c584
Valentin <valentin.louvet@supelec.fr>
2016-09-19 09:53:51 +0200

    update page recherche et création page planning

--- www/js/app.js
+++ www/js/app.js
@@ -61,15 +61,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+
 
 
     .state('tab.concert', {
@@ -145,8 +137,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'ProfilCtrl'
       }
     }
-  });
+  })
 
+  .state('tab.planning', {
+    url: '/profil/planning',
+    views: {
+      'tab-planning': {
+        templateUrl: 'templates/tab-planning.html',
+        controller: 'PlanningController'
+      }
+    }
+  });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/home');
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,11 +3,6 @@ angular.module('starter.controllers')
 .controller('HomeCtrl', function($scope) {})
 
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-
 
 .controller('AlbumCtrl', function($scope, $stateParams, Chats) {
 });
