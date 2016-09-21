df0ee7a052da8084cbd9ea08c004573cf22a27ff
tuyet <thi.vuong@student.ecp.fr>
2016-09-21 16:30:04 +0200

    Error handler for author page

--- www/js/auteur-controller.js
+++ www/js/auteur-controller.js
@@ -1,9 +1,5 @@
 angular.module('starter.controllers').controller('AuteurCtrl', function($scope, $http) {
-
-
-
 $scope.name= "goldman";
-$scope.songs=[];
 $scope.isSong=true;
 
 $http({
@@ -11,30 +7,33 @@ $http({
   url: 'http://localhost:8080/author?name='+$scope.name,
   header:{
   Origin:'http://localhost:8100'
-}
+  }
 }).then(function successCallback(response) {
-
-    $scope.answer = response.data;
-
-    for(var i = 0, len = $scope.answer.length; i < len; i++)
-    {
-
-        $scope.songs.push({
-            titre : $scope.answer[i].title,
-            iswc : $scope.answer[i].iswc
-
-        });
-
+    $scope.answer = response.data.works;
+    $scope.error= response.data.error;
+    $scope.isSong = false;
+    if ($scope.error==""){
+      if ($scope.answer.length !== 0){
+        $scope.isSong = true;
+        for(var i = 0, len = $scope.answer.length; i < len; i++) {
+          var temp = $scope.answer[i].title;
+          $scope.answer[i].title = temp.charAt(0).toUpperCase()+ temp.substring(1).toLowerCase();
+        }
+      }
+    } else {
+      //Show an alert of the error
+      var alertPopup = $ionicPopup.alert({
+        title: "Error !",
+        template: $scope.error
+      });
+      alertPopup.then(function(res) {
+        console.log($scope.error);
+      });
     }
-
-$scope.isSong = true;
-
-
   }, function errorCallback(error) {
-console.log(error);
-$scope.isSong = false;
-
-  });
+  console.log(error);
+  $scope.isSong = false;
+});
 
 
 
