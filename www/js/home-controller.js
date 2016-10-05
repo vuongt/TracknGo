d03b3ed087d6011b6b2a3e47481507d7c70b8e2f
Valentin <valentin.louvet@supelec.fr>
2016-10-05 15:06:57 +0200

    fix de l'update google map + google map se charge maintenant avec les lat/lng du serveur + google map est center sur la position rentrée dans GO

--- www/js/home-controller.js
+++ www/js/home-controller.js
@@ -1,6 +1,5 @@
 angular.module('starter.controllers')
-  .controller('HomeCtrl', function ($scope, $state, $cordovaGeolocation,$http, $ionicModal, AuthService, API_ENDPOINT,$stateParams,$ionicPopup) {
-
+  .controller('HomeCtrl', function ($scope, $state, $cordovaGeolocation, $http, $ionicModal, AuthService, API_ENDPOINT, $stateParams, $ionicPopup ) {
 
 
     //Initialisation des variables d'affichage
@@ -18,17 +17,27 @@ angular.module('starter.controllers')
       return [year, month, day].join('-');
     };
 
+
+
+
+
+
 //Chargement des concerts
     $scope.charging = true;
     $scope.isConcertHome = false;
-    $scope.lat="";
-    $scope.lng="";
-    $scope.radius="50";
-    $scope.start="";
-    $scope.end="";
-    $scope.timeCriteria="aujourd'hui";
-    if($stateParams.lat && $stateParams.lat!=="") {$scope.lat=$stateParams.lat; console.log($scope.lat)}
-    if($stateParams.lng && $stateParams.lng!=="") {$scope.lng=$stateParams.lng;}
+    $scope.lat = "";
+    $scope.lng = "";
+    $scope.radius = "50";
+    $scope.start = "";
+    $scope.end = "";
+    $scope.timeCriteria = "aujourd'hui";
+    if ($stateParams.lat && $stateParams.lat !== "") {
+      $scope.lat = parseFloat($stateParams.lat);
+    }
+    if ($stateParams.lng && $stateParams.lng !== "") {
+      $scope.lng = parseFloat($stateParams.lng);
+    }
+    console.log($scope.lat);
 
 
     if($stateParams.radius && $stateParams.radius!=="") {$scope.radius=$stateParams.radius;}
@@ -44,7 +53,7 @@ angular.module('starter.controllers')
   $scope.isConcertHome=false;
 $scope.isPlus=false;
 
-  },15000)
+  },15000);
 
 
 
@@ -58,12 +67,12 @@ $scope.isPlus=false;
 
       clearTimeout(attentionAuTemps);
       $scope.charging = false;
-      $scope.isPlus= false;
+      $scope.isPlus = false;
       $scope.answer = response.data;
 
 
       if ($scope.answer.error == "") {
-        if ($scope.lat ==""){
+        if ($scope.lat == "") {
           $scope.concerts = $scope.answer.concerts;
         } else $scope.concerts = $scope.answer.restrictedConcerts;
         console.log($scope.concerts);
@@ -72,11 +81,11 @@ $scope.isPlus=false;
           $scope.isConcertHome = true;
 
           if ($scope.numLimit <= $scope.concerts.length) {
-                          $scope.isPlus = true;
-                        }
+            $scope.isPlus = true;
+          }
 
         } else {
-          $scope.charging=false;
+          $scope.charging = false;
         }
         $scope.concerts.forEach(function (item, index) {
 
@@ -87,26 +96,26 @@ $scope.isPlus=false;
        item.NOM = item.NOM.charAt(0).toUpperCase() + item.NOM.substring(1).toLowerCase();
        item.VILLE = item.VILLE.charAt(0).toUpperCase() + item.VILLE.substring(1).toLowerCase();
 
-          if (item.TITRPROG == "Manifestation de _artiste a preciser ..."){
-              if(item.NOM=="Salle non referencee" || item.NOM==""||item.NOM==" . . ."){
-                 item.TITRPROG = "Manifestation @ "+item.VILLE;
+          if (item.TITRPROG == "Manifestation de _artiste a preciser ...") {
+            if (item.NOM == "Salle non referencee" || item.NOM == "" || item.NOM == " . . .") {
+              item.TITRPROG = "Manifestation @ " + item.VILLE;
 
-                 item.NOM="";
-              }
-              else{
-                  item.TITRPROG = "Manifestation @ "+item.NOM;
+              item.NOM = "";
+            }
+            else {
+              item.TITRPROG = "Manifestation @ " + item.NOM;
 
-                  item.NOM="";
-              }
-              }
-          if(item.ADR==" . . ."){
-                   item.ADR="";
+              item.NOM = "";
+            }
+          }
+          if (item.ADR == " . . .") {
+            item.ADR = "";
 
           }
-          else{
-              length = item.ADR.length;
-              item.ADR=item.ADR.slice(0,length-2);
-              }
+          else {
+            length = item.ADR.length;
+            item.ADR = item.ADR.slice(0, length - 2);
+          }
 
           item.DATDBTDIF = new Date(item.DATDBTDIF);
           //item.id_bit is undefined 'cause these concerts come from Eliza
@@ -114,133 +123,80 @@ $scope.isPlus=false;
 
 
         //initialisation google maps
-        var options = {timeout: 10000, enableHighAccuracy: true};
+        var init = function () {
 
-        $cordovaGeolocation.getCurrentPosition(options).then(function (position) {
-          var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-          console.log(latLng.lat());
-          if($scope.lat){
-            var mapOptions = {
-              center: {lat: $scope.lat, lng: $scope.lng},
-              zoom: 11,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-          } else {
-            var mapOptions = {
-              center: latLng,
-              zoom: 11,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-          }
+          var mapOptions = {
+            zoom: 11,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+
+          var div = document.getElementById('map');
+          console.log(div);
+
+          $scope.map = new google.maps.Map(div, mapOptions);
+
+          console.log(div);
+
+          var options = {timeout: 10000, enableHighAccuracy: true};
 
 
-          var geocoder = new google.maps.Geocoder();
-          $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-          //creation de la fonction isOpen a infowindow
-          function isInfoWindowOpen(infoWindow) {
-            var map = infoWindow.getMap();
-            return (map !== null && typeof map !== "undefined");
-          }
-          google.maps.event.addListenerOnce($scope.map, 'idle', function () {
 
-            $scope.concerts.forEach(function (item, index) {
-              setTimeout(function () {
+          var mapOptions = {
+            zoom: 11,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
 
 
-                var request = {
+          $cordovaGeolocation.getCurrentPosition(options).then(function (position) {
 
-                  query: item.NOM + " " + item.VILLE
-                };
+            if($scope.lat){
+            $scope.map.setCenter({ lat:$scope.lat, lng:$scope.lng});
+            } else {
+              var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+              $scope.map.setCenter(latLng);
+            }
 
-                var service = new google.maps.places.PlacesService($scope.map);
 
-                service.textSearch(request, callback);
+            console.log($scope.map.center.lat());
+            //creation de la fonction isOpen a infowindow
+            function isInfoWindowOpen(infoWindow) {
+              var map = infoWindow.getMap();
+              return (map !== null && typeof map !== "undefined");
+            }
 
-                function callback(results, status) {
-                  if (status == google.maps.places.PlacesServiceStatus.OK) {
-                    var marker = new google.maps.Marker({
-                      map: $scope.map,
-                      animation: google.maps.Animation.DROP,
-                      position: results[0].geometry.location
-                    });
+            google.maps.event.addListenerOnce($scope.map, 'idle', function () {
 
-                    var infoWindow = new google.maps.InfoWindow({
-                      content: item.TITRPROG + " @ " + item.NOM.toLowerCase()
-                    });
-                    google.maps.event.addListener(marker, 'click', function () {
-                      if (isInfoWindowOpen(infoWindow)) {
-                        infoWindow.close();
-                      }
-                      else {
-                        infoWindow.open($scope.map, marker);
-                      }
-                    });
+              $scope.concerts.forEach(function (item, index) {
+                var marker = new google.maps.Marker({
+                  map: $scope.map,
+                  animation: google.maps.Animation.DROP,
+                  position: {lat: parseFloat(item.LAT), lng : parseFloat(item.LNG)}
+                });
+                var infoWindow = new google.maps.InfoWindow({
+                  content: item.TITRPROG + " @ " + item.VILLE.toLowerCase()
+                });
+                google.maps.event.addListener(marker, 'click', function () {
+                  if (isInfoWindowOpen(infoWindow)) {
+                    infoWindow.close();
                   }
                   else {
-                    request = {
-                      query: item.VILLE
-
-                    };
-                    service.textSearch(request, function (results, status) {
-
-                      if (status === google.maps.places.PlacesServiceStatus.OK) {
-
-                        var marker = new google.maps.Marker({
-                          map: $scope.map,
-                          animation: google.maps.Animation.DROP,
-                          position: results[0].geometry.location
-                        });
-                        var infoWindow = new google.maps.InfoWindow({
-                          content: item.TITRPROG + " @ " + item.VILLE.toLowerCase()
-                        });
-                        google.maps.event.addListener(marker, 'click', function () {
-                          if (isInfoWindowOpen(infoWindow)) {
-                            infoWindow.close();
-                          }
-                          else {
-                            infoWindow.open($scope.map, marker);
-                          }
-                        });
-                      } else {
-                        console.log('Geocode was not successful for the following reason: ' + status);
-                      }
-                    });
+                    infoWindow.open($scope.map, marker);
                   }
 
-                }
+                });
 
-              }, 300*index);
+              });
+
+            }, function (error) {
             });
-            /*for (var i = 0; i < $scope.concerts.length; i++) {
-             (function () {
-             var address = "LA MAROQUINERIE IN PARIS"; //$scope.concerts[i].VILLE;
-             geocoder.geocode({'address': address}, function (results, status) {
-             if (status === google.maps.GeocoderStatus.OK) {
-             var marker = new google.maps.Marker({
-             map: $scope.map,
-             animation: google.maps.Animation.DROP,
-             position: results[0].geometry.location
-             });
-             var infoWindow = new google.maps.InfoWindow({
-             //content: concert.title
-             });
-             google.maps.event.addListener(marker, 'click', function () {
-             if (isInfoWindowOpen(infoWindow)) {
-             infoWindow.close();
-             }
-             else {
-             infoWindow.open($scope.map, marker);
-             }
-             });
-             } else {
-             alert('Geocode was not successful for the following reason: ' + status);
-             }
-             });
-             })();
-             }*/
-          }, function (error) {
           });
-        });
+
+          google.maps.event.trigger( $scope.map, 'resize');
+
+        };
+
+        init();
+
       }
 
     }, function errorCallback(response) {
@@ -253,27 +209,27 @@ $scope.isPlus=false;
     };
     $scope.removePlanning = function (cdeprog, id_bit) {
       console.log("removing concert from planning with id" + id_bit);
-      AuthService.delPlanning(cdeprog, id_bit,verifyAction);
+      AuthService.delPlanning(cdeprog, id_bit, verifyAction);
     };
-    $scope.isInPlanning = function(cdeprog, id_bit){
+    $scope.isInPlanning = function (cdeprog, id_bit) {
       return AuthService.isInPlanning(cdeprog, id_bit);
     };
-    function verifyAction(authorized,actionSucceed){
+    function verifyAction(authorized, actionSucceed) {
       if (!authorized) {
         var confirmPopup = $ionicPopup.confirm({
           title: 'Oups !',
           template: 'Please sign in to do this action',
-          okText:'Sign in'
+          okText: 'Sign in'
         });
 
-        confirmPopup.then(function(res) {
-          if(res) {
+        confirmPopup.then(function (res) {
+          if (res) {
             $state.go('tab.profil');
           } else {
             console.log('Action annulé');
           }
         });
-      } else if (!actionSucceed){
+      } else if (!actionSucceed) {
         var alertPopup = $ionicPopup.alert({
           title: "Oups !",
           template: "There is a problem while connecting to server. Please try again later"
@@ -285,44 +241,16 @@ $scope.isPlus=false;
         $scope.planning = AuthService.getPlanning(); // We don't use $scope.planning, but this action refresh the userPlanning in AuthService
       }
     }
-    //===========for the quicksearch modal
-    /*$ionicModal.fromTemplateUrl('templates/quicksearch.html', {
-      scope: $scope,
-      animation: 'slide-in-up'
-    }).then(function (modal) {
-      $scope.modal = modal;
-    });
 
-    $scope.openModal = function () {
-      $scope.modal.show();
+
+
+
+    $scope.printMore = function () {
+      $scope.numLimit = $scope.numLimit + 5;
+      if ($scope.numLimit > $scope.concerts.length) {
+        $scope.isPlus = false;
+      }
     };
-
-    $scope.closeModal = function () {
-      $scope.modal.hide();
-    };
-
-    //Cleanup the modal when we're done with it!
-    $scope.$on('$destroy', function () {
-      $scope.modal.remove();
-    });
-
-    // Execute action on hide modal
-    $scope.$on('modal.hidden', function () {
-      // Execute action
-    });
-
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function () {
-      // Execute action
-    });*/
-
-
-        $scope.printMore = function () {
-          $scope.numLimit = $scope.numLimit + 5;
-          if ($scope.numLimit > $scope.concerts.length) {
-            $scope.isPlus = false;
-          }
-        };
 
 
   });
