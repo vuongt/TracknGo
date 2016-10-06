432b8d8898a900c7666c399c4f4615d392db740b
tuyet <thi.vuong@student.ecp.fr>
2016-10-06 13:10:42 +0200

    Improve switching geolocation in research concerts

--- www/js/search-concert-controller.js
+++ www/js/search-concert-controller.js
@@ -5,52 +5,6 @@ angular.module('starter.controllers')
   .controller('SearchConcertCtrl',function($rootScope,$scope,$cordovaGeolocation, $ionicPopup,$state){
     var geocoder = new google.maps.Geocoder();
      $scope.charging=false;
-
-    var getDate = function(start,end){
-      if (start && end && start <= end){
-        $scope.start = start;
-        $scope.end = end;
-
-      } else {
-        //Show an alert that user has to choose a date
-        var alertPopup = $ionicPopup.alert({
-          title: "Oups !",
-          template: "Choissiez une date de début et une date de fin valide!"
-        });
-        alertPopup.then(function (res) {
-          console.log("Date entrée non valide");
-        });
-      }
-    };
-    var getLocation = function(locationString,geolocation){
-
-
-      if (geolocation){
-        var options = {timeout: 10000, enableHighAccuracy: true};
-        $cordovaGeolocation.getCurrentPosition(options).then(function (position) {
-          //$scope.location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-          $scope.lng= position.coords.latitude;
-          $scope.lat= position.coords.longitude;
-          console.log("Searching for your location");
-          console.log(position.coords.latitude, position.coords.longitude);
-        }, function(err) {
-          $ionicLoading.hide();
-          console.log(err);
-        });
-      } else {
-        geocoder.geocode( { 'address' : locationString}, function( results, status ) {
-          if( status == google.maps.GeocoderStatus.OK ) {
-          $scope.charging=true;
-            $scope.location= (results[0].geometry.location.lat(), results[0].geometry.location.lng())
-            $scope.lng=results[0].geometry.location.lng();
-            $scope.lat=results[0].geometry.location.lat();
-            console.log(results[0].geometry.location.lat(), results[0].geometry.location.lng());
-          } else {
-            alert( 'Geocode was not successful for the following reason: ' + status );
-          }
-        } );
-      }
-    };
     var getRadius = function(radius){
       if (radius){
         $scope.radius= radius; //en km
@@ -58,21 +12,22 @@ angular.module('starter.controllers')
         $scope.radius = "50";
       }
     };
-
-
     $scope.search = function(locationString,geolocation,radius,start,end){
       if (start && end && start <= end){
         $scope.start = start;
         $scope.end = end;
-        if (geolocation && locationString) {
+        if (!geolocation && !locationString) {
           //Show an alert that user has to choose a date
           var alert1 = $ionicPopup.alert({
-            title: "Attention !",
-            template: "L'option géolocalisation est activée!"
+            title: "Oups !",
+            template: "Vous n'avez pas défini le lieu de recherche !"
           });
           alertPopup.then(function (res) {
-            console.log("Geolocalisation et le lieu défini à la fois ");
+            console.log("without location");
           });
+        }
+        if (geolocation && locationString){
+          this.geolocation=false;
         }
         if (geolocation){
           $scope.charging=true;
@@ -117,5 +72,11 @@ angular.module('starter.controllers')
         });
       }
     };
+    $scope.geolocationOn= function (geolocation){
+      this.geolocation = true;
+    }
+    $scope.geolocationOff= function (geolocation){
+      this.geolocation = false;
+    }
   });
 
